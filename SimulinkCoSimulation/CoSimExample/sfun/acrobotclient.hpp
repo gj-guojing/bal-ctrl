abeebf388040cb1b71bc4aa997cd8a801e5af6b1
lidonghao <lidonghao_99@163.com>
2024-03-01 17:22:42 +0800

    add SimulinkCoSimulation

---
+++ SimulinkCoSimulation/CoSimExample/sfun/acrobotclient.hpp
@@ -0,0 +1,17 @@
+
+#ifndef ACROBOT_CLIENT_HPP
+#define ACROBOT_CLIENT_HPP
+
+void *setupruntimeresources_wrapper(const std::string & connStr);
+
+void start_wrapper(unsigned int *iter_ptr);
+
+void outputs_wrapper(void *zm, unsigned int *iter_ptr, double *y1_ptr, double *y2_ptr);
+
+void update_wrapper(void *zm, unsigned int *iter_ptr, const double input_data[], unsigned int size );
+
+void terminate_wrapper(void *zm, unsigned int *iter_ptr);
+
+void cleanupruntimeresouces_wrapper(void *zm);
+
+#endif // CONTROL_CLIENT_HPP
